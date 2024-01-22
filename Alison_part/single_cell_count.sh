8ff305778ff03136d5983064373d0f55d005ef2a
cristiancb94 <140175759+cristiancb94@users.noreply.github.com>
2024-01-22 11:52:14 +0100

    Update single_cell_count.sh

--- Alison_part/single_cell_count.sh
+++ Alison_part/single_cell_count.sh
@@ -1,27 +1,29 @@
-  GNU nano 2.9.8                                                                    cell_ranger_count_exercise.sh                                                                               
 #!/bin/bash
 
 #SBATCH --job-name=cellranger_count  # Job name
 #SBATCH --nodes=1                    # Number of nodes to use
 #SBATCH --cpus-per-task=16           # Number of CPU cores to allocate
-#SBATCH --mem=24000                  # Memory to allocate in MB
-#SBATCH --time=12:00:00               # Maximum time for the job to run (hh:mm:ss)
-#SBATCH --output=/scratch/course/2023w300106/barrera/cell_ranger_1.log
-#SBATCH --error=/scratch/course/2023w300106/barrera/cell_ranger_1.err
+#SBATCH --mem=24000                  # Memory to allocate in MB 
+#SBATCH --time=04:00:00               # Maximum time for the job to run (hh:mm:ss)
+#SBATCH --output=/scratch/course/2023w300106/barrera/cell_ranger_8d2.log
+#SBATCH --error=/scratch/course/2023w300106/barrera/cell_ranger_8d2.err
 #SBATCH --mail-type=END
 #SBATCH --mail-user=cristian.barrera@univie.ac.at
 
 #Loading modules
 
-mkdir
+mkdir 
 
 module load cellranger
 
 # Run cellranger count
 cellranger count \
-  --id cellranger_count_8d1n2 \
+  --id cellranger_count_8d2r1 \
   --transcriptome /scratch/course/2023w300106/barrera/genome_ref_nemato/
   --fastqs /scratch/molevo/agcole/course/datafiles/sequences_raw/8d2.run1 \
   --sample 64066 \
   --localcores 16 \
   --localmem 20 \
+  --chemistry threeprime \
+  --force-cells 10000 \
+  --nosecondary \
