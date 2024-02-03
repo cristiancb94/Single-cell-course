e546408fbab588aa82b1c693c7c393b620b6e38b
cristiancb94 <140175759+cristiancb94@users.noreply.github.com>
2024-02-03 14:03:58 +0100

    Update Star_ref.sh

--- Star_ref.sh
+++ Star_ref.sh
@@ -1,3 +1,16 @@
+#!/bin/bash
+
+#SBATCH --job-name=mkref_job
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=16   # Replace with the appropriate number of CPUs
+#SBATCH --mem=24000           # Replace with the appropriate amount of memory
+#SBATCH --time=2:00:00
+#SBATCH --output=/scratch/course/2023w300106/barrera/cellranger_ref.log
+#SBATCH --error=/scratch/course/2023w300106/barrera/cellranger_ref.err
+#SBATCH --mail-type=END
+#SBATCH --mail-user=cristian.barrera@univie.ac.at
+
+
 STAR --runMode genomeGenerate \
 --genomeDir jaNemVect1.1_STAR \
 --runThreadN 8 \
